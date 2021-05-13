2e80dee74b12656477a9a76de3985ff178260770
Masayuki SAKASAI <46712420+msakasai@users.noreply.github.com>
2021-05-13 22:36:42 +0900

    Update app.py
    スラッシュコマンド機能追加

--- functions/app.py
+++ functions/app.py
@@ -5,6 +5,7 @@ from jinja2 import Template
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.middleware_factory import lambda_handler_decorator
 from aws_lambda_powertools.utilities.data_classes import EventBridgeEvent
+from aws_lambda_powertools.utilities.data_classes import APIGatewayProxyEvent
 
 logger = Logger()
 
@@ -23,18 +24,34 @@ def middleware_before_after(handler, event, context):
 
 @middleware_before_after
 def lambda_handler(event, context):
+    """ EventBridge Handler """
     event: EventBridgeEvent = EventBridgeEvent(event)
-    mm_list_channels(event.detail)
+    mm_list_channels()
 
     return 0
 
+@middleware_before_after
+def api_handler(event, context):
+    """ API Gateway Handler """
+    # print(event)
+    event: APIGatewayProxyEvent = APIGatewayProxyEvent(event)
+    mm_list_channels(event['queryStringParameters'])
 
-def mm_list_channels(eventDetail: dict):
+    return {
+        'statusCode': 200,
+        'body': '{"message": "Channel list OK."}'
+    }
+
+
+def mm_list_channels(params: dict = {}):
+    # print(params)
     mm_channels = MmChannels(
-        os.getenv('MM_BASE_URL'),
-        os.getenv('MM_TEAM_ID'),
-        os.getenv('MM_POST_CHANNEL_ID'),
         os.getenv('MM_TOKEN'),
+        os.getenv('MM_BASE_URL'),
+        os.getenv('MM_TEAM_ID') if not params else params['team_id'],
+        os.getenv('MM_POST_CHANNEL_ID') if not params else params['channel_id'],
+        None if not params else params['user_id'],
+        False if not params else True
     )
     logger.info(mm_channels)
     
@@ -55,24 +72,29 @@ MM_POST_TEXT_TMPL = """
 class MmChannels:
 
     __slot__ = [
+        'token',
         'base_url',
         'team_id',
         'post_channel_id',
-        'token',
+        'user_id',
+        'ephemeral',
         'mm_channels_api_url',
+        'mm_post_api_url',
         'channels'
     ]
 
     def __str__(self):
-        return f'Public channel list post. base_url: {self.base_url}, team_id: {self.team_id}, post_channel_id: {self.post_channel_id}'
+        return f'Public channel list post. base_url: {self.base_url}, team_id: {self.team_id}, post_channel_id: {self.post_channel_id}, user_id: {self.user_id}, mm_channels_api_url: {self.mm_channels_api_url}, mm_post_api_url: {self.mm_post_api_url}'
 
-    def __init__(self, _base_url: str, _team_id: str, _post_channel_id: str, _token: str):
+    def __init__(self, _token: str, _base_url: str, _team_id: str, _post_channel_id: str, _user_id: str, _ephemeral: bool):
+        self.token = _token
         self.base_url = _base_url
         self.team_id = _team_id
         self.post_channel_id = _post_channel_id
-        self.token = _token
-        self.mm_channels_api_url = f'{_base_url}/api/v4/teams/{_team_id}/channels'
-        self.mm_post_api_url = f'{_base_url}/api/v4/posts'
+        self.user_id = _user_id
+        self.ephemeral = _ephemeral
+        self.mm_channels_api_url = f'{_base_url}/api/v4/teams/{self.team_id}/channels'
+        self.mm_post_api_url = f'{_base_url}/api/v4/posts' + ('/ephemeral' if self.ephemeral else '')
 
     def channel_list(self) -> None:
         _channel_list = []
@@ -122,10 +144,18 @@ class MmChannels:
             'Content-Type': 'application/json'
         }
 
-        params = {
+        _params = {
             'channel_id': self.post_channel_id,
             'message': _post_text,
         }
+        if self.ephemeral:
+            params = {
+                'user_id': self.user_id,
+                'post': _params
+            }
+        else:
+            params = _params
+        # print(params)
 
         response = requests.post(self.mm_post_api_url, headers=headers, json=params)
         if (response.status_code != 201):
